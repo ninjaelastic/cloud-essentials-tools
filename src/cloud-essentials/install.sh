f85862e2014233bf6a548f5e6ddb0ed46f030700
Mykola Lobanov <ninja@stardevs.gg>
2024-03-27 14:54:46 -0700

    pulumi

--- src/cloud-essentials/install.sh
+++ src/cloud-essentials/install.sh
@@ -559,6 +559,8 @@ if [ "${PULUMI_INSTALL}" = "true" ]; then
 
 fi
 
+
+
 if ! type docker > /dev/null 2>&1; then
     echo -e '\n(*) Warning: The docker command was not found.\n\nYou can use one of the following scripts to install it:\n\nhttps://github.com/microsoft/vscode-dev-containers/blob/main/script-library/docs/docker-in-docker.md\n\nor\n\nhttps://github.com/microsoft/vscode-dev-containers/blob/main/script-library/docs/docker.md'
 fi
